f503f4e7edb311e75f80a540856c18848fa3872b
fgken <fg.bell.tree.333@gmail.com>
2020-06-18 23:44:45 +0900

    libcurl: update multi

--- libcurl/multi.c
+++ libcurl/multi.c
@@ -5,32 +5,70 @@
 
 #include <curl/curl.h>
 
+static int
+add_new_handle(CURLM *multi_handle, const char *url)
+{
+    CURLMcode mc;
+    CURL *http_handle;
+
+    http_handle = curl_easy_init();
+    if (http_handle == NULL) {
+        fprintf(stderr, "Failed to curl_easy_init\n");
+        return -1;
+    }
+    curl_easy_setopt(http_handle, CURLOPT_URL, url);
+    mc = curl_multi_add_handle(multi_handle, http_handle);
+    if (mc != CURLM_OK) {
+        fprintf(stderr, "Failed to add http_handle to multi\n");
+        return -1;
+    }
+    return 0;
+}
+
 int
 main(void)
 {
+    int ret = 1;
     CURL *http_handle;
     CURLM *multi_handle;
-    int still_running = 1;
+    CURLcode res;
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    http_handle = curl_easy_init();
-    curl_easy_setopt(http_handle, CURLOPT_URL, "https://www.iij.ad.jp/");
+    res = curl_global_init(CURL_GLOBAL_DEFAULT);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "Failed to initialize libcurl: %s\n",
+                curl_easy_strerror(res));
+        goto end;
+    }
     multi_handle = curl_multi_init();
-    curl_multi_add_handle(multi_handle, http_handle);
+    if (multi_handle == NULL) {
+        fprintf(stderr, "Failed to initialize multi handle\n");
+        goto end;
+    }
 
-    while (still_running) {
+    add_new_handle(multi_handle, "https://google.com/");
+
+    while (1) {
         CURLMcode mc;
         int numfds;
+        int running_handles;
 
-        mc = curl_multi_perform(multi_handle, &still_running);
-
-        if (still_running) {
-            mc = curl_multi_poll(multi_handle, NULL, 0, 1000, &numfds);
-        }
-
+        fprintf(stderr, "TRACE: curl_multi_poll start\n");
+        mc = curl_multi_poll(multi_handle, NULL, 0, 3000, &numfds);
         if (mc != CURLM_OK) {
-            fprintf(stderr, "failed, code %d\n", mc);
+            fprintf(stderr, "Failed to curl_multi_poll: %s\n",
+                    curl_multi_strerror(mc));
+            // goto end;
         }
+        fprintf(stderr, "TRACE: curl_multi_poll end: numfds = %d\n", numfds);
+        fprintf(stderr, "TRACE: curl_multi_perform start\n");
+        mc = curl_multi_perform(multi_handle, &running_handles);
+        if (mc != CURLM_OK) {
+            fprintf(stderr, "Failed to curl_multi_perform: %s\n",
+                    curl_multi_strerror(mc));
+            // goto end;
+        }
+        fprintf(stderr, "TRACE: curl_multi_perform end: running_handles=%d\n",
+                running_handles);
     }
 
     curl_multi_remove_handle(multi_handle, http_handle);
@@ -38,5 +76,7 @@ main(void)
     curl_multi_cleanup(multi_handle);
     curl_global_cleanup();
 
-    return 0;
+    ret = 0;
+end:
+    return ret;
 }
